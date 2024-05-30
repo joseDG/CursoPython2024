cc5f5184db4574697e937712a62513abf5932055
JoseDG <josethp1@gmail.com>
2024-05-29 20:00:51 -0500

    Variables

---
+++ 04-Variables/Practica3.py
@@ -0,0 +1,9 @@
+"""
+PRACTICA 3
+
+Declara la variable curso, asígnale el valor "Python", 
+y muestra en pantalla la frase:
+"""
+
+curso = "Python"
+print("Estas tomando el curso: " + curso)
---
+++ 04-Variables/app.py
@@ -0,0 +1,17 @@
+nombre = "Juan"
+#print(nombre) 
+nombre = "Laura"
+nombre = "Jose"
+print(nombre)
+
+#definir bien las variables
+xx = "Tomate"
+print(xx)
+
+edad1 = 30
+edad2 = 60
+
+edadTotal = edad1 + edad2
+print("Edad Total: " + edadTotal)
+
+
