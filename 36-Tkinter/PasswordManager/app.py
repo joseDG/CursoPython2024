f52eada37f829de4d83dc611f9d9ae0e38b94e8e
JoseDG <josethp1@gmail.com>
2024-06-19 20:10:56 -0500

    tkinter finish

--- 36-Tkinter/PasswordManager/app.py
+++ 36-Tkinter/PasswordManager/app.py
@@ -1,6 +1,8 @@
 from tkinter import *
+from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
@@ -21,7 +23,57 @@ def generate_password():
   password_entry.insert(0, password)
   pyperclip.copy(password)
 
+# ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+
+  website = website_entry.get()
+  email = email_entry.get()
+  password = password_entry.get()
+  new_data = {
+    website: {
+      "email": email,
+      "password": password,
+    }
+  }
+
+  if len(website) == 0 or len(password) == 0:
+    messagebox.showinfo(title = "Oops", message="Por favor verificar los campos que no esten vacios.")
+
+  else:
+    try:
+      with open("data.json", "r") as data_file:
+        #lee la data
+        data = json.load(data_file)
+    except FileNotFoundError:
+      with open("data.json", "w") as data_file:
+        json.dump(new_data, data_file, indent=4)
+    else:
+      #update data
+      data.update(new_data)
+
+      with open("data.json", "w") as data_file:
+        #guarda la data actualiada
+        json.dump(data, data_file, indent=4)
+    finally:
+      website_entry.delete(0, END)
+      password_entry.delete(0, END)
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+  website = website_entry.get()
+  try:
+    with open("data.json") as data_file:
+      data = json.load(data_file)
+  except FileNotFoundError:
+    messagebox.showinfo(title="Error", message="Data no encontrada")
+  else:
+    if website in data:
+      email = data[website]["email"]
+      password = data[website]["password"]
+      messagebox.showinfo(title=website, message=f"Email: {email}\n Password: {password}")
+    else:
+      messagebox.showinfo(title="Error", message=f"No hay detalles {website} existente.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -52,11 +104,11 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-search_button = Button(text="Search", width=13)
+search_button = Button(text="Buscar", width=13, command=find_password)
 search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Guardar", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
